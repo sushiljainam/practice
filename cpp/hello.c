5f93c1b7647acc53267078742354900351a2dec1
Sushil Jain <sushil.jain@monotype.com>
2021-05-31 18:23:44 +0530

    loop tried with build system updated

--- cpp/hello.c
+++ cpp/hello.c
@@ -2,5 +2,17 @@
 
 int main(){
 	printf("hello world \n");
+	for (int i = 0; i < 5; ++i)
+	{
+		for (int j = 0; j < i; ++j)
+		{
+			printf(" ");
+		}
+		for (int j = i; j < 4; ++j)
+		{
+			printf("X");
+		}
+		printf("\n");
+	}
 	return 0;
 }
