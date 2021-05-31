a88a0f161870d1f29c7629525ae9c750a6d075ef
Sushil Jain <sushil.jain@monotype.com>
2021-05-31 18:25:33 +0530

    indent using space

--- cpp/hello.c
+++ cpp/hello.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 // {
-// 	"cmd": ["gcc", "$file_name", "-o", "${file_base_name}", "&&", "./${file_base_name}"],
-// 	"file_patterns": ["*.c"]
+//  "cmd": ["gcc", "$file_name", "-o", "${file_base_name}", "&&", "./${file_base_name}"],
+//  "file_patterns": ["*.c"]
 // }
 
 int main(){
-	printf("hello world \n");
-	for (int i = 0; i < 5; ++i)
-	{
-		for (int j = 0; j < i; ++j)
-		{
-			printf(" ");
-		}
-		for (int j = i; j < 4; ++j)
-		{
-			printf("X");
-		}
-		printf("\n");
-	}
-	return 0;
+    printf("hello world \n");
+    for (int i = 0; i < 5; ++i)
+    {
+        for (int j = 0; j < i; ++j)
+        {
+            printf(" ");
+        }
+        for (int j = i; j < 4; ++j)
+        {
+            printf("X");
+        }
+        printf("\n");
+    }
+    return 0;
 }
