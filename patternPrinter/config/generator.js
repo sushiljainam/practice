4ab3a2e614cfd9e057c61cd6ec0a527ec1a29392
Ekta Jain <ekta.jain@ranosys.com>
2021-06-03 00:29:02 +0530

    patternPrinter init

---
+++ patternPrinter/config/generator.js
@@ -0,0 +1,2 @@
+const letterCode = require('./samples/E');
+console.log(JSON.stringify(letterCode));
---
+++ patternPrinter/config/samples/E.js
@@ -0,0 +1,42 @@
+const fillWithP = (addItemProps) => Object.assign(
+    {},
+    {
+        type: 'item',
+        fill: 'P',
+    },
+    addItemProps,
+);
+const fillWithZ = (addItemProps) => Object.assign(
+    {},
+    {
+        type: 'item',
+        fill: 'Z',
+    },
+    addItemProps,
+);
+
+module.exports = {
+    type: 'row',
+    height: '5*N',
+    children: [
+        fillWithP({ width: 'N' }),
+        {
+            type: 'column',
+            width: '2*N',
+            children: [
+                fillWithP({ height: 'N' }),
+                fillWithZ({ height: 'N' }),
+                {
+                    type: 'row',
+                    height: 'N',
+                    children: [
+                        fillWithP({ width: '2*N-2' }),
+                        fillWithZ({ width: '2' }),
+                    ],
+                },
+                fillWithZ({ height: 'N' }),
+                fillWithP({ height: 'N' }),
+            ],
+        }
+    ],
+}
